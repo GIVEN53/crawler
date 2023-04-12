b9b5e458f93cfba86965b92f386970464d6c354d
NAM GIBEOM <rlqja0523@naver.com>
2023-04-12 13:22:51 +0900

    feat: 예외 처리

--- bot.py
+++ bot.py
@@ -15,8 +15,8 @@ async def on_ready():
     await bot.change_presence(activity=game, status=discord.Status.online)
 
     # 봇 구동 시 메시지 전송
-    notice_channel = bot.get_channel(loader.get_env('notice_channel_id'))
-    await notice_channel.send('Server start!\n' + strftime('%Y.%m.%d - %X (%a)'))
+    notice_channel = bot.get_channel(int(loader.get_env('notice_channel_id')))
+    await notice_channel.send(f'Server start!\n{get_date_time()}')
 
     # 봇 정보
     print(f'login sucess: {bot.user}')
@@ -28,23 +28,35 @@ async def on_ready():
 
     # 크롤링 시작
     now_number = '0'
-    alimi_channel = bot.get_channel(loader.get_env('alimi_channel_id'))
-    while True:
-        result = crawler.start(driver)
-        if result['number'] != now_number:
-            now_number = result['number']
-            message = mapping(result)
-            await alimi_channel.send(message)
-        sleep(5)
+    alimi_channel = bot.get_channel(int(loader.get_env('alimi_channel_id')))
+    try:
+        while True:
+            result = crawler.start(driver)
+            if result['number'] != now_number:
+                now_number = result['number']
+                message = mapping(result)
+                await alimi_channel.send(message)
+            sleep(5)
+    except KeyboardInterrupt as e:
+        await notice_channel.send(f'Server terminated.\n{get_date_time()}')
+        exit()
+    except RuntimeError as e:
+        await notice_channel.send(f'runtime error occured.\n\
+                                  Server closed.\n{get_date_time()}')
 
 
 def mapping(crawl_result: dict):
-    message = ''
+    message = '---------------------------------------\n'
     for key, value in crawl_result.items():
         if key == 'number':
             continue
         message += f'{key}: {value}\n'
-    return message.rstrip()
+    message += '---------------------------------------'
+    return message
+
+
+def get_date_time():
+    return f'Date: {strftime("%Y.%m.%d (%a)")}\nTime: {strftime("%X")}'
 
 
 if __name__ == '__main__':
