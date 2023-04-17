3634702ef3883fcb7ae0ccbd327c530954ffaadf
NAM GIBEOM <rlqja0523@naver.com>
2023-04-18 01:08:13 +0900

    feat: 미팅 정보 메시지

--- mapper.py
+++ mapper.py
@@ -1,3 +1,6 @@
+weekdays = {0: '월', 1: '화', 2: '수', 3: '목', 4: '금', 5: '토', 6: '일'}
+
+
 def mapping_new_info(crawl_result: dict):
     message = '---------------------------------------\n'
     for key, value in crawl_result.items():
@@ -5,4 +8,11 @@ def mapping_new_info(crawl_result: dict):
             continue
         message += f'{key}: {value}\n'
     message += '---------------------------------------'
+    return message
+
+
+def mapping_meeting_info(weekday_num, hour, min):
+    weekday = weekdays[weekday_num]
+    message = f'📢 현재 시각: {weekday}요일, {hour}시 {min}분\n미팅 1분 전입니다.'
+
     return message
