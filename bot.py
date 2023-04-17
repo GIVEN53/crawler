2c9e3a4839a3305b5a8ce9ce7e9d7f2dd7ee629d
NAM GIBEOM <rlqja0523@naver.com>
2023-04-18 00:12:08 +0900

    refactor: 알림 전송 loop 분리 (백그라운드 실행)

--- bot.py
+++ bot.py
@@ -3,51 +3,53 @@ import discord
 import crawler
 from time import strftime
 import mapper
-import asyncio
 from selenium.common.exceptions import UnexpectedAlertPresentException
+from discord.ext import tasks
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = discord.Client(intents=intents)
 REFRESH_SEC = 180
+now_number = '0'
+
 
 @bot.event
 async def on_ready():
+    global notice_channel, alimi_channel, driver
+
     # 봇 상태 정의
     game = discord.Game('크롤링')
     await bot.change_presence(activity=game, status=discord.Status.online)
 
     # 봇 구동 시 메시지 전송
     notice_channel = bot.get_channel(int(loader.get_env('notice_channel_id')))
+    alimi_channel = bot.get_channel(int(loader.get_env('alimi_channel_id')))
     await notice_channel.send(f'⭐ Server start!\n{get_date_time()}\n🔔 Notice: Refresh cycle is {REFRESH_SEC}s. 🔔')
 
-    # 크롤링 전 로그인
     driver = crawler.create_driver()
     crawler.login(driver)
-
-    # 크롤링 결과 알림 전송
-    alimi_channel = bot.get_channel(int(loader.get_env('alimi_channel_id')))
-    await send_result(notice_channel, alimi_channel, driver, '0')
+    send_result.start()
 
 
-async def send_result(notice_channel, alimi_channel, driver, now_number):
+# 크롤링 결과 알림 전송
+@tasks.loop(seconds=REFRESH_SEC)
+async def send_result():
+    global now_number
+
     try:
-        while True:
-            result = crawler.crawl_target(driver)
-            if result['number'] != now_number:
-                now_number = result['number']
-                message = mapper.mapping_new_info(result)
-                await alimi_channel.send(message)
-            await asyncio.sleep(REFRESH_SEC)
-    except UnexpectedAlertPresentException :
+        result = crawler.crawl_target(driver)
+        if result['number'] != now_number:
+            now_number = result['number']
+            message = mapper.mapping_new_info(result)
+            await alimi_channel.send(message)
+    except UnexpectedAlertPresentException:
         crawler.login(driver)
-        await send_result(alimi_channel, driver, now_number)
-    except asyncio.CancelledError or KeyboardInterrupt:
-        await notice_channel.send(f'⛔ Server terminated.\n{get_date_time()}')
+        await send_result()
 
 
-@bot.event
-async def on_error(event, *args, **kwargs):
+@send_result.after_loop
+async def close():
+    await notice_channel.send(f'⛔ Server terminated.\n{get_date_time()}')
     await bot.close()
 
 
